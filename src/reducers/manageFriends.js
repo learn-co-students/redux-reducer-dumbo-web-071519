e367bb33560fcfd967426ed04aca59482be735fe
github cjl248 <github cjl248@cornell.edu>
2020-02-27 17:21:52 -0500

    Done.

--- src/reducers/manageFriends.js
+++ src/reducers/manageFriends.js
@@ -1,2 +1,17 @@
-export function manageFriends(state, action){
+export function manageFriends(state = {friends: []}, action){
+  switch (action.type) {
+
+    case "ADD_FRIEND" :
+      return {friends: [...state.friends, action.friend]}
+
+    case "REMOVE_FRIEND" :
+      const newFriendArray = state.friends.filter((friend) => {
+        return action.id !== friend.id
+      })
+      return {friends: newFriendArray}
+
+    default:
+      return state
+
+  }
 }
